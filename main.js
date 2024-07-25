ccc6160123a58e56c785f5b27c6208c5dddc7654
joysdev <joyswork@protonmail.com>
2024-07-24 19:18:32 -0600

    Add comments to main. Comment out index button and form that should go "inside" Create New Sequence.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const newroutine = document.querySelector('#newroutine');
 
-// initialize button
+// initialize button this should actually be startroutine.onclick
 newroutine.onclick = goTimer;
 
 const options = [
