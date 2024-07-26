c28cdfa1d5fb8112a358f2cb0ca3cae427cb2828
joysdev <joyswork@protonmail.com>
2024-07-25 21:00:41 -0600

    Add new feature ideas to list in index, add inputs to createnewseq, add basic function description in comments at top of main.js.

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+/* If Equal Time per Pose is selected, user is presented with <input type="number">s  one for seconds (>=60), one for minutes (>=60), one for number of poses. Then they can choose a tone 
+(or color flash) for start, finish, and change pose. Then they can save & name, then they can return to main menu, or start routine. Main menu has buttons for new routine and saved routines. Saved routines has buttons for each saved routine, and a button for new routine. New routine has Saved Routines button, and form for inputs.
+
+
+
 const newroutine = document.querySelector('#newroutine');
 
 // initialize button this should actually be startroutine.onclick
