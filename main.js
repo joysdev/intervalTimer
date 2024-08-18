f27fe3b37e45d71ad88fc910aa10aba9c9798747
joysdev <joyswork@protonmail.com>
2024-08-18 17:38:22 -0600

    Paste code from http://kellylougheed.com/hiit/ into main comments.
    Add Remove a Pose button to customnewseq.
    
    Declare initial timer vars and add ID to start button.

--- main.js
+++ main.js
@@ -3,6 +3,17 @@ function addFn() {
     divEle.innerHTML += `<div id="poseTime"><input type="number" class="input" id="hour" min="0" max="24" step="1" placeholder="hr" onkeypress="return (event.charCode !=8 && event.charCode ==0 || (event.charCode >= 48 && event.charCode <= 57))"></input><input type="number" class="input" id="minutes" min="0" max="60" step="1" placeholder="min"></input><input type="number" class="input" id="seconds" min="0" max="60" step="1" placeholder="sec"></input></div>`;
 }
 
+var seconds
+var minutes
+var hours
+
+var cushion
+
+var startButton = document.getElementById("start");
+var pauseButton = document.getElementById("pause");
+
+
+
 /*function removeFn() {
     poseTime.remove();
 }
@@ -14,5 +25,86 @@ function addFn() {
 Main menu (index.html) has buttons for new sequence and saved sequences. Saved sequences has buttons for each saved sequence, and a button for new sequence. New sequence has Saved sequences button, and form for inputs.
 
  I want for when User clicks Submit to input number of poses in their custom sequence, they are presented with that number of time fields.
+
+
+window.onload = function() {
+
+  var seconds = 20;
+  var rest = true;
+  var interval;
+
+  var intervalTime = 20;
+  var breakTime = 10;
+
+  var settingsButton = document.getElementById("settings");
+  var intervalInput = document.getElementById("intervalTime");
+  var breakInput = document.getElementById("breakTime");
+
+  var startButton = document.getElementById("start");
+  var pauseButton = document.getElementById("pause");
+  var resetButton = document.getElementById("reset");
+
+  var statusHeader = document.getElementById("status");
+  var secondsSpan = document.getElementById("sec");
+
+  settingsButton.onclick = function() {
+    intervalTime = Math.floor(intervalInput.value * 1);
+    breakTime = Math.floor(breakInput.value * 1);
+    reset();
+  }
+
+  startButton.onclick = function() {
+    rest = false;
+    changeToGo();
+    interval = setInterval(countdownSeconds, 1000);
+  }
+
+  resetButton.onclick = function() {
+    reset();
+  }
+
+  function reset() {
+    clearInterval(interval);
+    seconds = intervalTime;
+    secondsSpan.innerText = seconds;
+    rest = true;
+    changeToRest();
+  }
+
+  pauseButton.onclick = function() {
+    clearInterval(interval);
+  }
+
+  function countdownSeconds() {
+    seconds -= 1;
+    secondsSpan.innerText = seconds;
+    checkForStateChange();
+  }
+
+  function checkForStateChange() {
+    if (seconds == 0 && rest == false) {
+      seconds = breakTime + 1;
+      rest = true;
+      changeToRest();
+    } else if (seconds == 0 && rest == true) {
+      seconds = intervalTime + 1;
+      rest = false;
+      changeToGo();
+    }
+  }
+
+  function changeToRest() {
+    $("body").css("background", "cyan");
+    statusHeader.innerText = "Rest";
+  }
+
+  function changeToGo() {
+    $("body").css("background", "pink");
+    statusHeader.innerText = "Go!";
+  }
+
+}
+
+
  */
 
